35d19b0edcdfb8bda6471d259f4323177fbd5119
qts-engineer <222491842+qts-engineer@users.noreply.github.com>
2025-11-02 21:59:25 -0800

    Fix build script for Node 20 and Vercel deployment

--- scripts/postbuild.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-// DEFINITIVE FIX: Completely disable react-snap to prevent Puppeteer errors
-// This script does nothing - react-snap will never run
-
-console.log('postbuild: Skipped (react-snap disabled)');
-process.exit(0);
-
